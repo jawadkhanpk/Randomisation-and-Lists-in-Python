1deab762dbe5eb4e3e0cdde2c8edb8eb79052a59
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-29 23:32:26 +0500

    Banker-Roulette-who-will-Pay-the-Bill

--- Banker-Roulette-who-will-Pay-the-Bill.py
+++ Banker-Roulette-who-will-Pay-the-Bill.py
@@ -1,7 +1,15 @@
-name_string = input("Give me everybody's name separated by comma: ")
-name = name_string.split(", ")
+import random
+name_string = input("Give me everybody's names separated by comma: ")
+names = name_string.split(", ")
 
-print(name)
+number_items = len(names)
 
-# if input is: Jawad, Adan, Danial
-# Output will be: ["Jawad", "Adan", "Danial"]
+choice = random.randint(0, number_items - 1)
+# added -1 because list index starts from 0
+person_who_will_pay = names[choice]
+print(person_who_will_pay + " is gonna buy meal today!")
+
+
+#simple way, while using random.choice functionally can skip code from line 5-10
+# person_who_will_pay = random.choice(names)
+# print(person_who_will_pay + " is gonna buy meal today!")
