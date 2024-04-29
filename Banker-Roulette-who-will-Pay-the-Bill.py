57e175c50df4c086fcb470506073971d083c5ba0
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-29 23:05:55 +0500

    Converting String in to a List using .split()

---
+++ Banker-Roulette-who-will-Pay-the-Bill.py
@@ -0,0 +1,7 @@
+name_string = input("Give me everybody's name separated by comma: ")
+name = name_string.split(", ")
+
+print(name)
+
+# if input is: Jawad, Adan, Danial
+# Output will be: ["Jawad", "Adan", "Danial"]
