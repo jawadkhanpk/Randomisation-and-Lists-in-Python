441263bb6c004e67b4c6628660522bb90a809b29
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-29 22:22:12 +0500

    Randomisation of Integer and Float in Python

--- Randomisation-in-Python.py
+++ Randomisation-in-Python.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import random
+
+# Random Integer
+random_integer = random.randint(1,10)
+print(random_integer)
+
+# Random Float, this will generate random number from 0.00.... to 0.99....
+random_float = random.random()
+print(random_float)
+
+# Random Float, this will generate random number from 0.00.... to 9.99....
+random_float_mul = random.random() * 10
+print(random_float_mul)
