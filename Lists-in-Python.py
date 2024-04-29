204eedaf998a19d68fdf05864bc27a109acf86be
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-29 22:55:23 +0500

    Lists in Python

---
+++ Lists-in-Python.py
@@ -0,0 +1,25 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
+                     "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island",
+                     "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois",
+                     "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
+                     "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado",
+                     "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma",
+                     "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america[0])
+# Output will be Delaware
+
+print(states_of_america[-1])
+# Output will be Hawaii
+
+# Amendment of list
+states_of_america[0] = "Delwareeee"
+print(states_of_america)
+
+# Appending a List
+states_of_america.append("Jawad Land")
+print(states_of_america)
+
+# Extending a List
+states_of_america.extend(["Angela Land", "Jack Bauer Land", "Adan Land"])
+print(states_of_america)
